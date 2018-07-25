ba7e1e501995df46d2dc0a91e1fd38d4d96ca8b0
Jon Evans <jon@rezendi.com>
2018-07-25 11:15:48 -0700

    Running off of cliquebait not ganache; moved deploy to async/await

--- ethereum/migrations/2_deploy_contracts.js
+++ ethereum/migrations/2_deploy_contracts.js
@@ -7,59 +7,35 @@ const YKCommunities = artifacts.require('YKCommunities.sol');
 const YKRewards = artifacts.require('YKRewards.sol');
 const YKarma = artifacts.require('YKarma.sol');
 
-function seed_data(yk) {
-  yk.addNewCommunity(0, 0x0, 'ykarma.com', '{"name":"Alpha Karma"}', 'alpha').then(() => {
-    yk.addNewAccount(1, 0, '{"name":"Jon"}', 'mailto:jon@rezendi.com').then(() => {
-      yk.addNewAccount(1, 0, '{"name":"Test"}', 'mailto:test@rezendi.com').then(() => {
-        yk.addNewAccount(1, 0, '{"name":"Test Two"}', 'mailto:test2@rezendi.com').then(() => {
-          yk.accountForId(1).then(() => {
-            yk.replenish(1).then(() => {
-              yk.accountForId(2).then(() => {
-                yk.replenish(2);
-              });
-            });
-          });
-        });
-      });
-    });
-  });
-}
-
 module.exports = (deployer, network, accounts) => {
   const owner = accounts[0];
-  deployer.deploy(strings, {from : owner}).then(() => {
-    deployer.deploy(SafeMath, {from : owner}).then(() => {
-      deployer.deploy(YKStructs, {from : owner}).then(() => {
-        deployer.deploy(YKTranches, {from : owner}).then(() => {
-          deployer.deploy(YKAccounts, {from : owner}).then(() => {
-            deployer.link(strings, YKTranches, YKAccounts).then(() => {
-              deployer.link(SafeMath, YKTranches).then(() => {
-                deployer.deploy(YKCommunities, {from : owner}).then(() => {
-                  deployer.deploy(YKRewards, {from : owner}).then(() => {
-                    deployer.deploy(YKarma, YKTranches.address, YKAccounts.address, YKCommunities.address, YKRewards.address, {from : owner}).then(() => {
-                      YKTranches.deployed().then((ykt) => {
-                        ykt.transferOwnership(YKarma.address, {from: owner});
-                        YKAccounts.deployed().then((yka) => {
-                          yka.transferOwnership(YKarma.address, {from : owner});
-                          YKCommunities.deployed().then((ykc) => {
-                            ykc.transferOwnership(YKarma.address, {from : owner});
-                            YKRewards.deployed().then((ykv) => {
-                              ykv.transferOwnership(YKarma.address, {from : owner});
-                              YKarma.deployed().then((yk) => {
-                                seed_data(yk);
-                              });
-                            });
-                          });
-                        });
-                      });
-                    });
-                  });
-                });
-              });
-            });
-          });
-        });
-      });
-    });
+  deployer.then(async () => {
+    await deployer.deploy(strings, {from : owner});
+    await deployer.deploy(SafeMath, {from : owner});
+    await deployer.deploy(YKStructs, {from : owner});
+    await deployer.deploy(YKTranches, {from : owner});
+    await deployer.deploy(YKAccounts, {from : owner});
+    await deployer.link(strings, YKTranches, YKAccounts);
+    await deployer.link(SafeMath, YKTranches);
+    await deployer.deploy(YKCommunities, {from : owner});
+    await deployer.deploy(YKRewards, {from : owner});
+    await deployer.deploy(YKarma, YKTranches.address, YKAccounts.address, YKCommunities.address, YKRewards.address, {from : owner});
+    const ykt = await YKTranches.deployed();
+    await ykt.transferOwnership(YKarma.address, {from: owner});
+    const yka = await YKAccounts.deployed();
+    await yka.transferOwnership(YKarma.address, {from : owner});
+    const ykc = await YKCommunities.deployed();
+    await ykc.transferOwnership(YKarma.address, {from : owner});
+    const ykv = await YKRewards.deployed();
+    await ykv.transferOwnership(YKarma.address, {from : owner});
+    const yk = await YKarma.deployed();
+    await yk.addNewCommunity(0, 0x0, 'ykarma.com', '{"name":"Alpha Karma"}', 'alpha');
+    await yk.addNewAccount(1, 0, '{"name":"Jon"}', 'mailto:jon@rezendi.com');
+    await yk.addNewAccount(1, 0, '{"name":"Test"}', 'mailto:test@rezendi.com');
+    await yk.addNewAccount(1, 0, '{"name":"Test Two"}', 'mailto:test2@rezendi.com');
+    await yk.accountForId(1);
+    await yk.replenish(1);
+    await yk.accountForId(2);
+    await yk.replenish(2);
   });
 };
--- ethereum/truffle-config.js
+++ ethereum/truffle-config.js
@@ -3,7 +3,7 @@ module.exports = {
     development: {
       host: '127.0.0.1',
       port: 8545,
-      network_id: '4'
+      network_id: '*'
     }
   }
 };
