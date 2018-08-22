d5dab1aff0475d01e8eee2b6483361bb57356a37
Jon Evans <jon@rezendi.com>
2018-08-22 16:15:53 -0700

    move all env variables to server

--- web/src/fbase/auth.js
+++ web/src/fbase/auth.js
@@ -8,7 +8,7 @@ var devActionCodeSettings = {
 };
 
 const prodActionCodeSettings = {
-  url: 'http://localhost/finishSignIn',
+  url: `${process.env.PRODUCTION_ROOT}/finishSignIn`,
   handleCodeInApp: true,
 };
 
@@ -19,7 +19,7 @@ var devLinkCodeSettings = {
 };
 
 const prodLinkCodeSettings = {
-  url: 'http://localhost/linkEmail',
+  url: `${process.env.PRODUCTION_ROOT}/linkEmail`,
   handleCodeInApp: true,
 };
 
