63e8a4ea9b4d6dd69c49b8b2cadb46cb5946057a
Jon Evans <jon@rezendi.com>
2018-12-31 13:24:07 -0800

    fix readme link

--- web/src/components/Readme.js
+++ web/src/components/Readme.js
@@ -10,7 +10,7 @@ class Readme extends React.Component {
           <p>Welcome to YKarma!</p>
           <p>YKarma is an experimental project to model reputation as a spendable currency.</p>
           <p>The basic concept: every person in a community or organization is allotted 100 "karma coins" to distribute each week. These must be given away to other people before they can be used. The recipients can then spend these coins on various rewards (a day off, a conference ticket, a coffee with someone notable, etc.)</p>
-          <p>For more information, see <a href="https://github.com/rezendi/ykarma/blob/master/HOWTO.md">the project README</a> or email <a href="mailto:info@ykarma.com">info@ykarma.com</a>.</p>
+          <p>For more information, see <a href="https://github.com/rezendi/ykarma/blob/master/README.md">the project README</a> or email <a href="mailto:info@ykarma.com">info@ykarma.com</a>.</p>
         </Col>
       </Row>
     );
