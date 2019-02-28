126be5f8ff3df27d98e709384c671b58a485c517
Jon Evans <jon@rezendi.com>
2019-02-28 12:24:40 -0800

    ready to reload

--- server/cron/cron.js
+++ server/cron/cron.js
@@ -160,6 +160,7 @@ function sendReplenishSlack(account) {
   if (process.env.NODE_ENV === "test" || !account.urls) return;
   var slackUrl = null;
   const urls = account.urls.split("||");
+  // TODO: what to do about accounts with multiple Slack URLs? Notify all? Notify latest, as per current code, might be best...
   for (var i in urls) {
     if (urls[i].startsWith("slack:")) {
       slackUrl = urls[i];
