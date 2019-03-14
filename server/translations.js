f25bb77076029f427ffc2b46f70e141d35cab897
Jon Evans <jon@rezendi.com>
2019-03-13 20:39:35 -0700

    added translations

---
+++ server/translations.js
@@ -0,0 +1,58 @@
+const resources = {
+  en: {
+    translation: {
+      "Not authorized" : "Not authorized",
+      "Not logged in" : "Not logged in",
+      "existing" : "existing",
+      "Invalid URL" : "Invalid URL",
+      'Closed community, can only give to' : 'Closed community, can only give to',
+      "emails and/or via Slack" : "emails and/or via Slack",
+      
+      // Slack
+      "help" : "help",
+      "ykarma_is" : "YKarma is a reputation cryptocurrency which you give to others, who can then use it to buy rewards. You currently have",
+      "karma to give away to others, and" : "karma to give away to others, and",
+      "to_spend_on" : 'to spend on rewards. To send karma, use this slash command; for example, to send 10 karma to Alice with the message _for being awesome_, just type "/yk 10 to @alice for being awesome"',
+      "Sent!" : "Sent",
+      "Sending…" : "Sending…",
+      "Sorry! Valid amount not found" : "Sorry! Valid amount not found",
+      "Sorry! Valid recipient not found" : "Sorry! Valid recipient not found",
+      "Sorry! Your YKarma account is not set up for sending here" : "Sorry! Your YKarma account is not set up for sending here",
+      "Sorry! You don't have enough YKarma to do that. Your balance is" : "Sorry! You don't have enough YKarma to do that. Your balance is",
+      "Sorry! That YKarma account is not set up for receiving here" : "Sorry! That YKarma account is not set up for receiving here",
+      "balance" : "balance",
+      "bot_help" : "You can check your balance with 'balance', send with 'send' eg 'send 10 to @alice for being awesome', view available rewards with 'rewards', or purchase a reward with 'purchase' eg 'purchase 23'",
+      "Error getting available-to-spend amount" : "Error getting available-to-spend amount",
+      "You currently have a total of" : "You currently have a total of",
+      "to spend… You can get an itemization by flavor with the 'flavors' command" : "to spend… You can get an itemization by flavor with the 'flavors' command",
+      "You currently have" : "You currently have",
+      "to give away" : "to give away",
+      "send" : "send",
+      "has sent you" : "has sent you",
+      "karma" : "karma",
+      "with the message" : "with the message",
+      "rewards" : "rewards",
+      'Fetching available rewards from the blockchain…' : 'Fetching available rewards from the blockchain…',
+      "Error getting rewards" : "Error getting rewards",
+      "No rewards available" : "No rewards available",
+      "Available Rewards" : "Available Rewards",
+      "Error getting list of rewards" : "Error getting list of rewards",
+      "purchase" : "purchase",
+      "Please tell me the ID of the reward you want to purchase" : "Please tell me the ID of the reward you want to purchase",
+      "Attempting purchase…" : "Attempting purchase…",
+      "flavors" : "flavors",
+      "Sorry, I didn't understand you. You can ask for help with 'help'" : "Sorry, I didn't understand you. You can ask for help with 'help'",
+      
+      // Emails
+      
+      
+    }
+  },
+  kr: {
+    translation: {
+        
+    }
+  }
+};
+
+module.exports = resources;
