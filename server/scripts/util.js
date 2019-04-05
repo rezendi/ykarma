fccb39ab79edb78fef8f707eb83bccc6e446008c
Jon Evans <jon@rezendi.com>
2019-04-04 17:56:19 -0700

    double barreled util function

--- server/scripts/util.js
+++ server/scripts/util.js
@@ -14,11 +14,24 @@ eth.web3.eth.getAccounts().then((ethAccounts) => {
     console.log("blockNumber", bn);
     blockNumber = bn;
     doTheThing();
+    doTheOtherThing();
   });
 });
 
 function doTheThing() {
   //var method = eth.contract.methods.mergeAccounts(31,32);
+  var method = eth.contract.methods.mergeAccounts(31,32);
+  method.send({from:fromAccount, gas: eth.GAS}).on('error', (error) => {
+    console.log('error running', method._method);
+    console.log('send error', error);
+  }).on('confirmation', (number, receipt) => {
+    if (number === 1) {
+      console.log('merged!');
+    }
+  });
+}
+
+function doTheOtherThing() {
   var method = eth.contract.methods.mergeAccounts(24,22);
   method.send({from:fromAccount, gas: eth.GAS}).on('error', (error) => {
     console.log('error running', method._method);
