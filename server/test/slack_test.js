fea9c7e994485d174597c39f82dd7e9cf375d087
Jon Evans <jon@rezendi.com>
2019-04-08 09:54:21 -0700

    async await tests

--- server/test/slack_test.js
+++ server/test/slack_test.js
@@ -10,71 +10,55 @@ var TestCookies = [];
 
 describe('Slack', function () {
 
-  it('test open channel and post', async function (done) {
+  it('test open channel and post', async function () {
     var slackUrl = "slack:TEST-USER1";
     var text = `Mocha testing open channel and post`;
     slack.openChannelAndPost(slackUrl, text);
-    setTimeout(async () => {
-      try {
-        var res = await api.get('/api/slack/lastOpenConversation');
-        var last = JSON.parse(res.text).last;
-        expect(last.users).to.equal("USER1");
-        expect(last.token).to.equal("test");
-        res = api.get('/api/slack/lastPostMessage');
-        console.log("test", res.text);
-        last = JSON.parse(res.text).last;
-        expect(last.text.split(" ")[0]).to.equal("Mocha");
-        expect(last.channel).to.equal("TestChannel");
-        expect(last.token).to.equal("test");
-        res = api.get('/api/slack/lastPostMessage');
-        done();
-      } catch(err) {
-        done(err);
-      }
-    }, 500);
+    await sleep(500);
+    try {
+      var res = await api.get('/api/slack/lastOpenConversation');
+      var last = JSON.parse(res.text).last;
+      expect(last.users).to.equal("USER1");
+      expect(last.token).to.equal("test");
+      res = await api.get('/api/slack/lastPostMessage');
+      last = JSON.parse(res.text).last;
+      expect(last.text.split(" ")[0]).to.equal("Mocha");
+      expect(last.channel).to.equal("TestChannel");
+      expect(last.token).to.equal("test");
+      res = await api.get('/api/slack/lastPostMessage');
+      return true;
+    } catch(err) {
+      console.log("error", err);
+    }
   });
 
   // assumes a reward added as part of deploy
-  it('send karma in-channel and via DM', function (done) {
+  it('send karma in-channel and via DM', async function() {
     this.timeout(10000);
-    api.get('/api/accounts/setup?ykid=4').expect(200).end((err, res) => {
-      if (err) done (err);
+    try {
+      var res = await api.get('/api/accounts/setup?ykid=4');
       TestCookies = (res.headers['set-cookie'] || ['']).pop().split(';');
-      // send in channel
-      api.post('/api/slack/yk')
-        .send({"team_id":"TEST", "user_id":"USER1", "text":"send 1 to <@USER2|test2> just testing"})
-        .set('Cookie', TestCookies).expect(200)
-        .end(function (err, res) {
-          if (err) done (err);
-          expect(JSON.parse(res.text).text).to.equal("Sending…");
-          // send via YKBot
-          api.post('/api/slack/event')
-            .send({ "team_id":"TEST", "event": {"channel":"TestChannel", "type":"message.im", "user":"USER1", "text":"send 1 to <@USER2|test2> still testing"} })
-            .set('Cookie', TestCookies).expect(200)
-            .end(function (err, res) {
-              if (err) done (err);
-              expect(JSON.parse(res.text).text).to.equal("Sending…");
-              // list rewards
-              api.post('/api/slack/event')
-                .send({ "team_id":"TEST", "event": {"channel":"TestChannel", "type":"message.im", "user":"USER1", "text":"rewards"} })
-                .set('Cookie', TestCookies).expect(200)
-                .end(function (err, res) {
-                  if (err) done (err);
-                  expect(JSON.parse(res.text).text).to.equal("Fetching available rewards from the blockchain…");
-                  api.post('/api/slack/event')
-                    .send({ "team_id":"TEST", "event": {"channel":"TestChannel", "type":"message.im", "user":"USER1", "text":"purchase 1"} })
-                    .set('Cookie', TestCookies).expect(200)
-                    .end(function (err, res) {
-                      if (err) done (err);
-                      expect(JSON.parse(res.text).text).to.equal("Attempting purchase…");
-                      // TODO check the various posts to /testOpenConversation and /testPostMessage for correctness
-                      done();
-                    });
-                });
-            });
-        });
-    });
+      res = await api.post('/api/slack/yk').set('Cookie', TestCookies)
+        .send({"team_id":"TEST", "user_id":"USER1", "text":"send 1 to <@USER2> just testing"});
+      expect(JSON.parse(res.text).text).to.equal("Sending…");
+      // send via YKBot
+      res = await api.post('/api/slack/event').set('Cookie', TestCookies)
+        .send({ "team_id":"TEST", "event": {"channel":"TestChannel", "type":"message.im", "user":"USER1", "text":"send 1 to <@USER2|test2> still testing"} });
+      expect(JSON.parse(res.text).text).to.equal("Sending…");
+      // list rewards
+      res = await api.post('/api/slack/event').set('Cookie', TestCookies)
+        .send({ "team_id":"TEST", "event": {"channel":"TestChannel", "type":"message.im", "user":"USER1", "text":"rewards"} });
+      expect(JSON.parse(res.text).text).to.equal("Fetching available rewards from the blockchain…");
+      res = await api.post('/api/slack/event').set('Cookie', TestCookies)
+        .send({ "team_id":"TEST", "event": {"channel":"TestChannel", "type":"message.im", "user":"USER1", "text":"purchase 1"} })
+      expect(JSON.parse(res.text).text).to.equal("Attempting purchase…");
+    } catch(err) {
+      console.log("error", err);
+    }
   });
 
 });
-  
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
